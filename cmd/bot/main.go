5a82adf7cd480095486236915891b02f2415f209
Ilya <kozhuhov.1992@gmail.com>
2022-04-25 16:53:36 +0300

    add Names map

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -9,13 +9,13 @@ import (
 	"syscall"
 )
 
-var Store = make(map[string]bool)
-
 var (
 	Token    = flag.String("t", "", "Bot token")
 	GuildID  = flag.String("gi", "", "Guild id")
 	ChanelId = flag.String("ci", "", "Chanel id, for message")
 	ErrorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	Store    = make(map[string]bool)
+	Names    = make(map[string]string)
 )
 
 func init() {
@@ -46,18 +46,21 @@ func CheckState(s *discordgo.Session, event *discordgo.VoiceStateUpdate) {
 	newStore := make(map[string]bool)
 	for _, vs := range g.VoiceStates {
 		if !Store[vs.UserID] {
-			name := ""
+			if Names[vs.UserID] == "" {
+				user, err := s.User(vs.UserID)
+				checkErr(err)
+				Names[vs.UserID] = user.Username
+			}
 			for _, member := range g.Members {
 				if member.User.ID == vs.UserID {
-					name = member.User.Username
 					if member.Nick != "" {
-						name = member.Nick
+						Names[vs.UserID] = member.Nick
 					}
 				}
 
 			}
 
-			_, err := s.ChannelMessageSend(*ChanelId, "Привет "+name)
+			_, err := s.ChannelMessageSend(*ChanelId, "Привет "+Names[vs.UserID])
 			checkErr(err)
 		}
 		newStore[vs.UserID] = true
